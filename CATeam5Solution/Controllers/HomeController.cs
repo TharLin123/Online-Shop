1d0af078f1d94ddd713dcf5bc6a839595f804d90
TharLin123 <tharlinhtet97@gmail.com>
2021-10-12 11:58:37 +0630

    TLH Commit

--- CATeam5Solution/Controllers/HomeController.cs
+++ CATeam5Solution/Controllers/HomeController.cs
@@ -18,8 +18,15 @@ namespace CATeam5Solution.Controllers
             _logger = logger;
         }
 
+        List<Products> shoppingCart = new List<Products>();
+        public void AddToCart(int ProductId)
+        {
+           
+        }
+
         public IActionResult Index()
         {
+            ViewData["ShoppingCartProductCount"] = shoppingCart.Count;
             return View(); // testing github
         }
 
