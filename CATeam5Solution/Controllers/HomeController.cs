4ebef1512996876a3e61617d7be997c9e8673f9f
le8025 <e0838431@u.nus.edu>
2021-10-12 20:09:48 +0800

    *Hardcode product data into list of products under IActionResult Index() cause cannot figure out the database thing yet. *Drafted parts of the View for Gallery Page (=Index)

--- CATeam5Solution/Controllers/HomeController.cs
+++ CATeam5Solution/Controllers/HomeController.cs
@@ -20,6 +20,64 @@ namespace CATeam5Solution.Controllers
 
         public IActionResult Index()
         {
+            /* Hardcoding products as a list here first because
+             * still not very sure how to implement the Seed() function 
+             * and getting the data into Controller then View
+             * (XH)
+             */
+           
+            List<Products> allProducts = new List<Products>();
+
+            allProducts.Add(new Products
+            {
+                ProductID = 1,
+                Name = "PhotoEditShop",
+                UnitPrice = 39.99,
+                Description = "This app provides you with the capabilities to do professional design and photo editing!"
+            });
+
+            allProducts.Add(new Products
+            {
+                ProductID = 2,
+                Name = "readPDF Pro",
+                UnitPrice = 9.99,
+                Description = "Pro Version of the readPDF! Allows you to edit, encrypt and Sign off in a pdf document!  "
+            });
+
+            allProducts.Add(new Products
+            {
+                ProductID = 3,
+                Name = "ProPremier​",
+                UnitPrice = 59.99,
+                Description = "Premium version of the premier video editing application!  "
+            });
+
+            allProducts.Add(new Products
+            {
+                ProductID = 4,
+                Name = "RoomLight​",
+                UnitPrice = 39.99,
+                Description = "A creative image manipulation software that allows you to create a workflow so ease your life as both an avid or pro photographer   "
+            });
+
+            allProducts.Add(new Products
+            {
+                ProductID = 5,
+                Name = "EffectsAfter",
+                UnitPrice = 69.99,
+                Description = " VFX and Motion graphics software for the budding animators and 3d designers!  "
+            });
+
+            allProducts.Add(new Products
+            {
+                ProductID = 6,
+                Name = "illustrator",
+                UnitPrice = 69.99,
+                Description = "the industry-standard vector graphics app that lets you create logos, icons, drawings, typography, and complex illustrations for any medium!  "
+            });
+
+
+            ViewData["AllProducts"] = allProducts;
             return View(); // testing github
         }
 
