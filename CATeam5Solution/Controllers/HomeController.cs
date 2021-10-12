2c0bc7e475a0a47304a6e816296046c17b26bf63
le8025 <e0838431@u.nus.edu>
2021-10-13 03:44:51 +0800

    Got the DB part working for the Index(=Gallery Page).

--- CATeam5Solution/Controllers/HomeController.cs
+++ CATeam5Solution/Controllers/HomeController.cs
@@ -7,79 +7,40 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace CATeam5Solution.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        /*Have to comment out the following HomeController default
+         * Otherwise cannot put in DBContext and constructor
+         * which then results in not being able to add all products 
+         * to the list (XH)
+         */
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        private DBContext dbContext;
+        public HomeController(DBContext dbContext)
         {
-            _logger = logger;
+            this.dbContext = dbContext;
         }
 
-        List<Products> shoppingCart = new List<Products>();
+        //List<Products> shoppingCart = new List<Products>();
         public void AddToCart(int ProductId)
         {
            
         }
 
         public IActionResult Index()
-        {
-            /*Sorry hardcoding the products as a list here first
-             * because still trying to figure out the database part
-             * (XH)
+        { 
+            /*Gallery Page should be Home Page so not changing the name
              */
-            List<Products> allProducts = new List<Products>();
-
-            allProducts.Add(new Products
-            {
-                ProductID = 1,
-                ProductName = "PhotoEditShop",
-                UnitPrice = 39.99,
-                Description = "This app provides you with the capabilities to do professional design and photo editing!"
-            });
-
-            allProducts.Add(new Products
-            {
-                ProductID = 2,
-                ProductName = "readPDF Pro",
-                UnitPrice = 9.99,
-                Description = "Pro Version of the readPDF! Allows you to edit, encrypt and Sign off in a pdf document!  "
-            });
-
-            allProducts.Add(new Products
-            {
-                ProductID = 3,
-                ProductName = "ProPremier​",
-                UnitPrice = 59.99,
-                Description = "Premium version of the premier video editing application!  "
-            });
-
-            allProducts.Add(new Products
-            {
-                ProductID = 4,
-                ProductName = "RoomLight​",
-                UnitPrice = 39.99,
-                Description = "A creative image manipulation software that allows you to create a workflow so ease your life as both an avid or pro photographer   "
-            });
-
-            allProducts.Add(new Products
-            {
-                ProductID = 5,
-                ProductName = "EffectsAfter",
-                UnitPrice = 69.99,
-                Description = " VFX and Motion graphics software for the budding animators and 3d designers!  "
-            });
-
-            allProducts.Add(new Products
-            {
-                ProductID = 6,
-                ProductName = "illustrator",
-                UnitPrice = 69.99,
-                Description = "the industry-standard vector graphics app that lets you create logos, icons, drawings, typography, and complex illustrations for any medium!  "
-            });
-            
+            List<Products> allProducts = dbContext.Products.ToList();
 
             ViewData["AllProducts"] = allProducts;
             return View(); // testing github
