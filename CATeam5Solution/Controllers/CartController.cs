052b336025f32b80b16215ae175c739e477460e4
ongeeloon <ee_loon94@hotmail.com>
2021-10-13 21:39:15 +0800

    deleted CartTestController and CartTestProduct view

--- CATeam5Solution/Controllers/CartController.cs
+++ CATeam5Solution/Controllers/CartController.cs
@@ -21,7 +21,30 @@ namespace CATeam5Solution.Controllers
         {
             return View();
         }
-        public IActionResult ViewCart(List<Products> shoppingCart)
+        [Route("Cart")] // Route here first to test view
+        public IActionResult ViewCart()
+        {
+            Dictionary<string, int> ShoppingCartDict = new Dictionary<string, int>();
+            ShoppingCartDict.Add("1", 3);
+            ShoppingCartDict.Add("2", 4);
+            ShoppingCartDict.Add("3", 1);
+
+            Dictionary<Products, int> ShoppingCartDictReal = new Dictionary<Products, int>();
+            foreach (var item in ShoppingCartDict)
+            {
+                int id = Int32.Parse(item.Key);
+                //Change to fetch from DB instead
+                Products ProductToAdd = ProductList.ProductsListt.SingleOrDefault(r => r.ProductID == id);
+                ShoppingCartDictReal.Add(ProductToAdd, item.Value);
+            }
+
+            ViewData["ShoppingCartDict"] = ShoppingCartDictReal;
+            ViewData["TestImage"] = "/img/Behance-64.png"; //seed product image. need to add in products model.
+            return View();
+        }
+
+
+            public IActionResult ViewCart(List<Products> shoppingCart)
         {
             Session session = GetSession();
             if (session == null)
