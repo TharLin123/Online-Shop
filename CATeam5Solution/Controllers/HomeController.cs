4403ac16c64e59603b633edb7c3ebf8bd7fe292f
Yuchen Xin <e0838400@u.nus.edu>
2021-10-13 12:39:23 +0800

    My purchase test

--- CATeam5Solution/Controllers/HomeController.cs
+++ CATeam5Solution/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace CATeam5Solution.Controllers
         {
             return View();
         }
+        
+        public IActionResult MyPurchase()
+        {
+            return View();
+        }//for testing the page only now
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
