785588083b8fc86df2132b50d2088052c3091813
Yuchen Xin <e0838400@u.nus.edu>
2021-10-13 23:52:07 +0800

    Passing the history data to the view of Mypurchase

--- CATeam5Solution/Controllers/MyPurchaseController.cs
+++ CATeam5Solution/Controllers/MyPurchaseController.cs
@@ -20,18 +20,7 @@ namespace CATeam5Solution.Controllers
         {
             //check order status and login status,both true, jump to this page
             //select specific order to check order details
-            return View();
-        }
-        public IActionResult Details()
-        {
-            //Show orderId, ordertotal, email, phonenumber, orderDate, which account, coupon or not, activation code and something else?
-            //have function to delete orders
-            return View();
-        }
-
-        public IActionResult MyPurchase()
-        {
-            //在这里获取View中需要的所有数据
+            
             //Session session = GetSession();
             //if (session == null)
             //{
@@ -39,10 +28,10 @@ namespace CATeam5Solution.Controllers
             //}
 
             /*Guid userid = session.UserId;*///when the login function is done
-            Users user = dbContext.Users.FirstOrDefault(x => x.UserName == "adam");
+            Users user = dbContext.Users.FirstOrDefault(x => x.UserName.Equals("adam"));
             Guid userid = user.Id;
-            
-            List<Order> orders = dbContext.Orders.Where(x => x.UsersId == userid).ToList();
+
+            List<Order> orders = dbContext.Orders.Where(x => x.UsersId.Equals(userid)).ToList();
             List<ActCode> actCodes = new List<ActCode>();
             List<Products> products = new List<Products>();
             foreach (Order order in orders)
@@ -57,17 +46,24 @@ namespace CATeam5Solution.Controllers
                         products.Add(product);//get how many types of product he has bought 获取到他购买过几种商品(包括展示需要的各种信息)
                 }
             }
-            //Get all orders of this user contains some list<product> need to put together to compute the total types of product/ total num of each product
-            //orderdate---->2021-10-13 21:02:00.6758862 
-            //actcode list for each product of this order
-
-            ViewData["orderhistory"] = orders;//主要从这里获取订单时间 可以展示该商品最后购买时间
+            ////Get all orders of this user contains some list<product> need to put together to compute the total types of product/ total num of each product
+            ////orderdate---->2021-10-13 21:02:00.6758862 
+            ////actcode list for each product of this order
+            ViewData["orders"] = orders;//主要从这里获取订单时间 可以展示该商品最后购买时间
             ViewData["actcodes"] = actCodes;
             ViewData["products"] = products;
+            return View();
+        }
+        public IActionResult Details()
+        {
+            //Show orderId, ordertotal, email, phonenumber, orderDate, which account, coupon or not, activation code and something else?
+            //have function to delete orders
+            return View();
+        }
 
-            //Guid userId = new Guid();//test only
-            //GetHistory getHistory = new GetHistory(userId, dbContext);
-            //ViewData["gethistory"] = getHistory;
+        public IActionResult MyPurchase()
+        {
+            
             return View();
         }//for testing the page only now
         public IActionResult Review()
