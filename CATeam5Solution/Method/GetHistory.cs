7402dd043ccfcc85b182d7caac53a035a714d070
Yuchen Xin <e0838400@u.nus.edu>
2021-10-13 14:40:53 +0800

    gethistorydata method is under construction

---
+++ CATeam5Solution/Method/GetHistory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CATeam5Solution.Method
+{
+    public class GetHistory
+    {
+    }
+}
