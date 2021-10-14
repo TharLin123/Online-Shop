423f13ad41b5d9e655f14c732458de62ebe61bad
Yuchen Xin <e0838400@u.nus.edu>
2021-10-14 20:57:16 +0800

    1. Open the Session for Mypurchase and 2. rebuild the method to pass data to Mypurchase view 3. delete some useless view of Mypurchase 4. comment Yuchen's hardcode part of seeding cartItem

--- CATeam5Solution/Controllers/MyPurchaseController.cs
+++ CATeam5Solution/Controllers/MyPurchaseController.cs
@@ -18,40 +18,23 @@ namespace CATeam5Solution.Controllers
         }
         public IActionResult Index()
         {
-            //check order status and login status,both true, jump to this page
-            //select specific order to check order details
-            
-            //Session session = GetSession();
-            //if (session == null)
-            //{
-            //    return RedirectToAction("Index", "Login");
-            //}
+            //check order status and login status, both true, jump to this page
+            // select specific order to check order details
 
-            /*Guid userid = session.UserId;*///when the login function is done
-            Users user = dbContext.Users.FirstOrDefault(x => x.UserName.Equals("adam"));
-            Guid userid = user.Id;
-
-            List<Order> orders = dbContext.Orders.Where(x => x.UsersId.Equals(userid)).ToList();
-            List<ActCode> actCodes = new List<ActCode>();
-            List<Products> products = new List<Products>();
-            foreach (Order order in orders)
+            Session session = GetSession();
+            if (session == null)
             {
-                foreach (ActCode actcode in order.ActCodes)
-                {
-                    actCodes.Add(actcode);//get all actcodes of him 获取他所有的Actcodes(从这里可以计算出单个商品有几个Actcode)
-                }
-                foreach (Products product in order.Products)
-                {
-                    if (!products.Contains(product))
-                        products.Add(product);//get how many types of product he has bought 获取到他购买过几种商品(包括展示需要的各种信息)
-                }
+                return RedirectToAction("Index", "Login");
             }
-            ////Get all orders of this user contains some list<product> need to put together to compute the total types of product/ total num of each product
-            ////orderdate---->2021-10-13 21:02:00.6758862 
-            ////actcode list for each product of this order
-            ViewData["orders"] = orders;//主要从这里获取订单时间 可以展示该商品最后购买时间
-            ViewData["actcodes"] = actCodes;
-            ViewData["products"] = products;
+
+            Guid userid = session.UsersId;//get user's Guid
+            //the following 2 lines are for testing only
+            //Users user = dbContext.Users.FirstOrDefault(x => x.UserName.Equals("adam"));
+            //Guid userid = user.Id;
+            GetMyPurchaseData myPurchaseMaker = new GetMyPurchaseData(userid, dbContext);
+            List<MyPurchaseViewModel> myPurchaseViewModels = myPurchaseMaker.MakeMyPurchaseView();
+            ViewData["myPurchaseViewModel"] = myPurchaseViewModels;
+
             return View();
         }
         public IActionResult Details()
