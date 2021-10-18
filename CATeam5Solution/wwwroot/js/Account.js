4f2289a21bdf1f83545388cfc6ecab333c4066c0
Zhi Jie Lee <e0838442@u.nus.edu>
2021-10-18 13:09:07 +0800

    update accountjs

--- CATeam5Solution/wwwroot/js/Account.js
+++ CATeam5Solution/wwwroot/js/Account.js
@@ -6,26 +6,26 @@ var checkpass = function ()
     var password = document.getElementById('password').value;
     var confirmPass = document.getElementById('confirmPass').value;
     var username = document.getElementById('userName').value;
+    var password2 = document.getElementById('password').value.length;
 
-    if (password.toLowerCase() == username.toLowerCase())
+    if (password2 < 8) {
+        alert("Password requires to be at least 8 letters");
+        document.getElementById("submitCreate").disabled = true;
+        return;
+    }
+    else if (password.toLowerCase() == username.toLowerCase())
     {
         alert("Password cannot be the same as username!");
         document.getElementById("submitCreate").disabled = true;
         return;
     }
-    if (password == "")
+    else if (password == "")
     {
         alert("Please Enter Password!");
        document.getElementById("submitCreate").disabled = true;
         return;
     }
-    if (password < 8)
-    {
-        alert("Password requires to be at least 8 letters");
-        document.getElementById("submitCreate").disabled = true;
-        return;
-    }
-    if (password.toLowerCase() != confirmPass.toLowerCase())
+    else if (password.toLowerCase() != confirmPass.toLowerCase())
     {
         alert("Password & Confirm Password Do not match");
         document.getElementById("submitCreate").disabled = true;
@@ -35,6 +35,8 @@ var checkpass = function ()
     {
         alert("Password good to go!");
         document.getElementById("submitCreate").disabled = false;
+        document.getElementById("password").disabled = false;
+        document.getElementById("confirmPass").disabled = false;
     }
 
 }
